2115cbc16dac00dc4cb9b0debabdc9efc8679125
Xun Luan <xun.luan@walmartlabs.com>
2019-08-23 10:50:13 -0700

    change version

--- pycocoevalcap/tokenizer/ptbtokenizer.py
+++ pycocoevalcap/tokenizer/ptbtokenizer.py
@@ -15,7 +15,7 @@ import tempfile
 import itertools
 
 # path to the stanford corenlp jar
-STANFORD_CORENLP_3_4_1_JAR = 'stanford-corenlp-3.4.1.jar'
+STANFORD_CORENLP_3_4_1_JAR = 'stanford-corenlp-3.6.0.jar'
 
 # punctuations to be removed from the sentences
 PUNCTUATIONS = ["''", "'", "``", "`", "-LRB-", "-RRB-", "-LCB-", "-RCB-", \
